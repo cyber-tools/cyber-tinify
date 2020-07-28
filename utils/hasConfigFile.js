d18fa6162bee51563a938f148b10569b869eb158
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-28 17:42:05 +0800

    2020年07月28日 17:04的快速提交

--- utils/hasConfigFile.js
+++ utils/hasConfigFile.js
@@ -1,16 +1,13 @@
-const fs = require("fs");
 const path = require("path");
-const { promisify } = require("es6-promisify");
+const pathExists = require("path-exists")
+
 
 module.exports = async () => {
   try {
     const configFile = path.resolve(process.cwd(), "tinify.config.js");
-    await promisify(fs.access)(configFile, fs.constants.F_OK | fs.constants.W_OK);
-    return true;
+    const hasConfigFile = await pathExists(configFile);
+    return hasConfigFile;
   } catch (error) {
-    if (error.code === "ENOENT") {
-      return false;
-    };
     throw error;
-  }
+  };
 };
